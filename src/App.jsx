ab7e6d53c62ed6a5cc843fdd4845f1c92a23c03f
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-10-21 19:42:16 +0530

    error handled and add vercel to fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-// App.js
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import HelpPage from './pages/HelpPage';
 import ProfilePage from './pages/ProfilePage';
 import LoginPage from './pages/LoginPage';
+import ErrorPage from './pages/ErrorPage';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element:<PrivateRoute element={< Dashboard/>} />
+      element: <PrivateRoute element={<Dashboard />} />,
+      errorElement: <ErrorPage />
     },
     {
       path: "login",
-      element: <LoginPage />
-      ,
+      element: <LoginPage />,
+      errorElement: <ErrorPage />
     },
     {
       path: "/profile",
-      element: (
-        <PrivateRoute element={<ProfilePage />} />
-      ),
+      element: <PrivateRoute element={<ProfilePage />} />,
+      errorElement: <ErrorPage />
     },
     {
       path: "/help",
-      element: (
-        <PrivateRoute element={<HelpPage />} />
-      ),
+      element: <PrivateRoute element={<HelpPage />} />,
+      errorElement: <ErrorPage />
     },
+    {
+      // Catch all other routes that don't match
+      path: "*",
+      element: <ErrorPage />,
+    }
   ]);
 
   return <RouterProvider router={router} />;
